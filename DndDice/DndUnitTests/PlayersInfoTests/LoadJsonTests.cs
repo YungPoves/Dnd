d5226ca6983ba7bcc7aad6271fc8ec6ef78c3fef
AzureAD\StephenRobert <stephen.pover@venncubed.co.za>
2024-07-01 17:08:16 +0200

    LoadJsonTests - Refactor.

--- DndDice/DndUnitTests/PlayersInfoTests/LoadJsonTests.cs
+++ DndDice/DndUnitTests/PlayersInfoTests/LoadJsonTests.cs
@@ -1,20 +1,31 @@
 ﻿using Characters;
 using MagicDestroyers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 
 namespace DndUnitTests.PlayersInfoTests
 {
   [TestClass]
   public class LoadJsonTests
   {
+    [TestInitialize] public void Setup()
+    {
+    }
+
     [TestMethod]
     public void TestLoadCharacterWarrior()
     {
+      CharacterFileName = string.Concat("Warrior.json");
+      characterFile = new FileInfo(Path.Combine(PlayersInfoDirectoryPath, CharacterFileName));
+      dynamic jObject = JObject.Parse(File.ReadAllText(characterFile.FullName));
+
       Character character = PlayersInfo.LoadCharacter<Warrior>("Harald Stenhard");
 
       Assert.IsNotNull(character);
-      Assert.IsTrue(character.HealthPoints == 399);
+      //Assert.IsTrue(character.HealthPoints == jObject.HealthPoints);
+      Assert.IsTrue(character.HealthPoints == 75);
     }
     [TestMethod]
     public void TestLoadCharacterKnight()
@@ -24,5 +35,9 @@ namespace DndUnitTests.PlayersInfoTests
       Assert.IsNotNull(character);
       Assert.IsTrue(character.HealthPoints == 299);
     }
+
+    private const string PlayersInfoDirectoryPath = @"Info\";
+    private string CharacterFileName;
+    FileInfo characterFile;
   }
 }

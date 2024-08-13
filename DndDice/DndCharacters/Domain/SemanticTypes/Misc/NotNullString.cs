31768ae112e2c69de9a28b5a3621d42d7ef61d17
YungPoves <povesr12@gmail.com>
2024-08-13 15:48:01 +0200

    Added SemanticTypes/NotNullString

---
+++ DndDice/DndCharacters/Domain/SemanticTypes/Misc/NotNullString.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DndCharacters.Domain.SemanticTypes.Misc
+{
+  public abstract class NotNullString
+  {
+    [DebuggerStepThrough]
+    protected NotNullString(string value) : this(value, MaxLength)
+    {
+    }
+
+    [DebuggerStepThrough]
+    protected NotNullString(string value, int maxLength) : this(value, 1, maxLength) 
+    {
+    }
+
+    [DebuggerStepThrough]
+    protected NotNullString(string value, int minLength, int maxLength)
+    {
+      if (value == null)
+        throw new ArgumentNullException(nameof(value));
+
+      if (IsValid(value, minLength, maxLength) == false)
+        throw new ArgumentException($"Invalid value: {value}");
+
+      Value = value.Trim();
+    }
+
+    public static bool IsValid(string candidate)
+    {
+      return IsValid(candidate, MaxLength);
+    }
+    public static bool IsValid(string candidate, int maxLength)
+    {
+      return IsValid(candidate, 1, MaxLength);
+    }
+    public static bool IsValid(string candidate, int minLength, int maxLength)
+    {
+      if (candidate == null)
+        return false;
+
+      if(string.IsNullOrWhiteSpace(candidate))
+        return false;
+
+      if (candidate.Length > maxLength)
+        return false;
+
+      if (candidate.Length < minLength)
+        return false;
+      
+      return true;
+    }
+
+    public virtual string Value { get; protected set; }
+
+    private const int MaxLength = 2048;
+  }
+}

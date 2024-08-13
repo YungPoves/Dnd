845d514050378504beec9b9fb6a8366995469cec
YungPoves <povesr12@gmail.com>
2024-08-13 17:29:11 +0200

    Added SemanticTypes/CharacterName

---
+++ DndDice/Common/Domain/SemanticTypes/Characters/CharacterName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Common.SemanticTypes
+{
+  public class CharacterName : NotNullString
+  {
+    public const int MinLength = 1;
+    public const int MaxLength = 50;
+
+    public static new bool IsValid(string candidate)
+    {
+      return NotNullString.IsValid(candidate);
+    }
+
+    [DebuggerStepThrough]
+    public CharacterName(string value) : base(value, MinLength, MaxLength)
+    {
+      if (IsValid(value) == false)
+        throw new FormatException($"Invalid value: {value}");
+    }
+  }
+}

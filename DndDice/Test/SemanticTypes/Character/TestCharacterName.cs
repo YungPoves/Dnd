9aef180ae8f39a158c73ba930c7ffe57ef8aa7d7
YungPoves <povesr12@gmail.com>
2024-08-14 14:42:45 +0200

    Added Test project.

---
+++ DndDice/Test/SemanticTypes/Character/TestCharacterName.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Common.SemanticTypes;
+
+namespace Test.SemanticTypes
+{
+  [TestClass]
+  public class TestCharacterName
+  {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Null()
+    {
+      CharacterName obj = new CharacterName(null);
+
+      Assert.IsTrue(obj.Value == null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Empty()
+    {
+      CharacterName obj = new CharacterName(string.Empty);
+
+      Assert.IsTrue(obj.Value == string.Empty);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void TooLong()
+    {
+      string value = "-".PadRight(257);
+
+      CharacterName obj = new CharacterName(value);
+
+      Assert.IsTrue(obj.Value == null);
+    }
+
+    [TestMethod]
+    public void OK()
+    {
+      CharacterName obj = new CharacterName("value");
+
+      Assert.IsTrue(obj.Value == "value");
+    }
+  }
+}

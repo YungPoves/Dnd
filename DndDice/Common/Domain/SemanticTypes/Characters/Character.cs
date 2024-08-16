4628fe1d3b9d64ed4c0d995a962472fb9d5d842a
YungPoves <povesr12@gmail.com>
2024-08-16 11:08:07 +0200

    Added Character.cs

---
+++ DndDice/Common/Domain/SemanticTypes/Characters/Character.cs
@@ -0,0 +1,23 @@
+﻿namespace Common.SemanticTypes
+{
+  public class Character
+  {
+    public Character(string characterName)
+    {
+      this.Name = new CharacterName(characterName);
+    }
+
+    private CharacterName characterName;
+    private string characterRace;
+    private string characterclass;
+
+    public CharacterName Name
+    {
+      get => characterName;
+      set => characterName = value; 
+    }
+
+    public string Race { get => characterRace; set => characterRace = value; }
+    public string Characterclass { get => characterclass; set => characterclass = value; }
+  }
+}
